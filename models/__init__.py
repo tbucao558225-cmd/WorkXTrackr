7ffb95b3c1eeb650f17f15105faec025eb5099b2
tbucao558225-cmd <t.bucao.558225@umindanao.edu.ph>
2026-02-21 19:30:33 +0800

    Delete models directory

--- models/__init__.py
+++
@@ -1,8 +0,0 @@
-# models/__init__.py
-from .user import User
-from .attendance_data import Attendance
-from .employee_data import Employee
-from .announcement_data import Announcement
-from .request_data import Request
-
-__all__ = ['User', 'Attendance', 'Employee', 'Announcement', 'Request']
